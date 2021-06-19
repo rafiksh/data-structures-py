e1c28279adfdd6c87562010f175836037603bb9c
Rafik Al Shmoury <49872889+rafiksh@users.noreply.github.com>
2021-06-19 09:15:35 +0300

    feat: ex1 ex2

---
+++ Exercise-1.py
@@ -0,0 +1,6 @@
+def calculate_sum(a, b):
+    return a + b
+
+
+if __name__ == '__main__':
+    print(calculate_sum(3, 4))
---
+++ Exercise-2.py
@@ -0,0 +1,6 @@
+def is_square_root(x, y):
+    return x ** (1/2) == y
+
+
+if __name__ == '__main__':
+    print(is_square_root(3, 4))
